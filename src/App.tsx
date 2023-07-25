cee577b46520d16fa59e8de90f6e6808bf922e39
劻 <LedoBodzaW0913@gmail.com>
2023-07-25 22:53:59 +0800

    App.tsx Update
    1.Todo 改成從 types.ts import
    2.Todos[]會從localstorage撈資料
    3.使用useEffect 當更動完todos會重新儲存localstorage
    4.time改成Date
    5.filter從外部引入 改成固定變數

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import Filter from "./components/Filter";
@@ -6,26 +6,33 @@ import Search from "./components/Search";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { v4 as uuidv4 } from "uuid";
-
-interface Todo {
-  id: string;
-  content: string;
-  time: string;
-  person: string;
-  completed: boolean;
-  deleted: boolean;
-}
+import { Todo } from "./types/types";
+import { formatDate } from "./types/convertdate";
+import {
+  FilterType,
+  FILTER_COMPLETED,
+  FILTER_DELETED,
+  FILTER_UNCOMPLETED,
+} from "./types/filtertype";
 
 const App: React.FC = () => {
+  const initialTodos: Todo[] = JSON.parse(
+    localStorage.getItem("localTodos") || "[]"
+  );
   // 待辦事項列表的狀態
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
   // 過濾狀態
-  const [filter, setFilter] = useState<string>("");
+  const [filter, setFilter] = useState<FilterType>();
   // 搜尋關鍵字的狀態
   const [searchKeyword, setSearchKeyword] = useState<string>("");
 
+  // 每當 todos 狀態改變時更新 localStorage
+  useEffect(() => {
+    localStorage.setItem("localTodos", JSON.stringify(todos));
+  }, [todos]);
+
   // 新增待辦事項
-  const addTodo = (content: string, time: string, person: string) => {
+  const addTodo = (content: string, time: Date, person: string) => {
     const newTodo: Todo = {
       id: uuidv4(), // 使用uuid生成唯一的id
       content,
@@ -35,8 +42,6 @@ const App: React.FC = () => {
       deleted: false,
     };
     setTodos((prevTodos) => [...prevTodos, newTodo]);
-    localStorage.setItem("localTodos", JSON.stringify([...todos, newTodo]));
-    // console.log("localStorage:", localStorage.getItem("localTodos"));
   };
 
   // 切換待辦事項的完成狀態
@@ -46,7 +51,6 @@ const App: React.FC = () => {
         todo.id === id ? { ...todo, completed: !todo.completed } : todo
       )
     );
-    // console.log("switch");
   };
 
   // 刪除待辦事項
@@ -56,7 +60,6 @@ const App: React.FC = () => {
         todo.id === id ? { ...todo, deleted: true } : todo
       )
     );
-    // console.log("delete");
   };
 
   // 復原已刪除的待辦事項
@@ -66,14 +69,13 @@ const App: React.FC = () => {
         todo.id === id ? { ...todo, deleted: false } : todo
       )
     );
-    // console.log("restore");
   };
 
   // 更新待辦事項的內容、時間和人物
   const updateTodo = (
     id: string,
     content: string,
-    time: string,
+    time: Date,
     person: string
   ) => {
     setTodos((prevTodos) =>
@@ -81,44 +83,36 @@ const App: React.FC = () => {
         todo.id === id ? { ...todo, content, time, person } : todo
       )
     );
-    // console.log("update");
   };
 
   // 過濾待辦事項
-  const filterTodos = (filterType: string) => {
+  const filterTodos = (filterType: FilterType) => {
     setFilter(filterType);
-    // console.log("filter");
   };
 
   // 搜尋待辦事項
   const searchTodo = (keyword: string) => {
     setSearchKeyword(keyword);
-    // console.log("search");
   };
 
   // 根據過濾條件和搜尋關鍵字篩選待辦事項
   const filteredTodos = todos.filter((todo) => {
     const matchFilter =
-      filter === "completed"
+      filter === FILTER_COMPLETED
         ? todo.completed && !todo.deleted
-        : filter === "uncompleted"
+        : filter === FILTER_UNCOMPLETED
         ? !todo.completed && !todo.deleted
-        : filter === "deleted"
+        : filter === FILTER_DELETED
         ? todo.deleted
         : !todo.deleted;
 
     const matchSearch =
       todo.content.includes(searchKeyword) ||
-      todo.time.includes(searchKeyword) ||
+      formatDate(new Date(todo.time)).includes(searchKeyword) ||
       todo.person.includes(searchKeyword);
 
     return matchFilter && matchSearch;
   });
-
-  // 處理拖放事件的函式，接收更新後的 todos 陣列
-  const handleDrop = (updatedTodos: Todo[]) => {
-    setTodos(updatedTodos);
-  };
 
   return (
     <DndProvider backend={HTML5Backend}>
@@ -137,7 +131,7 @@ const App: React.FC = () => {
           deleteTodo={deleteTodo}
           updateTodo={updateTodo}
           restoreTodo={restoreTodo}
-          onDrop={handleDrop} // 傳遞 handleDrop 函式給 TodoList
+          setTodos={setTodos}
         />
       </div>
     </DndProvider>
