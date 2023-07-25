0c36d04ccdb8a6cc3588c60f75a04be3d156a987
劻 <LedoBodzaW0913@gmail.com>
2023-07-25 23:03:46 +0800

    new file:converdate.ts、filtertype.ts、types.ts
    新增的檔案

---
+++ src/types/filtertype.ts
@@ -0,0 +1,7 @@
+export type FilterType = "all" | "completed" | "uncompleted" | "deleted";
+
+// 定義常數，避免硬編碼
+export const FILTER_ALL: FilterType = "all";
+export const FILTER_COMPLETED: FilterType = "completed";
+export const FILTER_UNCOMPLETED: FilterType = "uncompleted";
+export const FILTER_DELETED: FilterType = "deleted";
