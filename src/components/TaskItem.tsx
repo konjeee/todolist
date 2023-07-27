68a21f2c18362fa70cd41821ede7dcd3474cc25b
劻 <LedoBodzaW0913@gmail.com>
2023-07-27 17:19:00 +0800

    TaskItem.tsx update
    1.移除Update功能

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDrag, useDrop } from "react-dnd";
 import { Todo } from "../types/types";
 import { formatDate } from "../types/convertdate";
 
 interface TaskItemProps {
-  todo: {
-    id: string;
-    content: string;
-    time: Date;
-    person: string;
-    completed: boolean;
-    deleted: boolean;
-  };
+  todo: Todo;
   todos: Todo[];
   toggleTodo: (id: string) => void;
   deleteTodo: (id: string) => void;
-  updateTodo: (id: string, content: string, time: Date, person: string) => void;
+  onEditTodo: (todo: Todo) => void;
   restoreTodo: (id: string) => void;
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
@@ -25,52 +18,24 @@ const TaskItem: React.FC<TaskItemProps> = ({
   todos,
   toggleTodo,
   deleteTodo,
-  updateTodo,
+  onEditTodo,
   restoreTodo,
-  setTodos
+  setTodos,
 }) => {
-  const [editing, setEditing] = useState(false);
-  const [updatedContent, setUpdatedContent] = useState(todo.content);
-  const [updatedTime, setUpdatedTime] = useState(todo.time);
-  const [updatedPerson, setUpdatedPerson] = useState(todo.person);
-
   const handleToggle = () => {
     toggleTodo(todo.id);
   };
 
   const handleEdit = () => {
-    setEditing(true);
+    onEditTodo(todo);
   };
 
   const handleDelete = () => {
     deleteTodo(todo.id);
   };
 
-  const handleSave = () => {
-    if (
-      updatedContent.trim() !== "" &&
-      updatedTime.toString().trim() !== "" &&
-      updatedPerson.trim() !== ""
-    ) {
-      setEditing(false);
-      updateTodo(todo.id, updatedContent, updatedTime, updatedPerson);
-    }
-  };
-
-  const handleCancel = () => {
-    setEditing(false);
-    setUpdatedContent(todo.content);
-    setUpdatedTime(todo.time);
-    setUpdatedPerson(todo.person);
-  };
-
   const handleRestore = () => {
     restoreTodo(todo.id);
-  };
-
-  const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedDate = new Date(e.target.value);
-    setUpdatedTime(selectedDate);
   };
 
   const [{ isDragging }, drag] = useDrag(() => ({
@@ -90,13 +55,13 @@ const TaskItem: React.FC<TaskItemProps> = ({
         if (draggedItem !== droppedItem) {
           let draggedIndex = todos.indexOf(draggedItem);
           let droppedIndex = todos.indexOf(droppedItem);
-          
-          if(draggedIndex !== -1 && droppedIndex !== -1){
+
+          if (draggedIndex !== -1 && droppedIndex !== -1) {
             let changetodos = todos;
             let temp = changetodos[draggedIndex];
             changetodos[draggedIndex] = changetodos[droppedIndex];
             changetodos[droppedIndex] = temp;
-            setTodos(() => changetodos)
+            setTodos(() => changetodos);
           }
         }
       },
@@ -109,46 +74,28 @@ const TaskItem: React.FC<TaskItemProps> = ({
   );
 
   return (
-    <li ref={(node) => drag(drop(node))} style={{ opacity: isDragging ? 0.5 : 1 }}>
+    <li
+      ref={(node) => drag(drop(node))}
+      style={{ opacity: isDragging ? 0.5 : 1 }}
+    >
       <input type="checkbox" checked={todo.completed} onChange={handleToggle} />
-      {editing ? (
-        <>
-          <input
-            type="text"
-            value={updatedContent}
-            onChange={(e) => setUpdatedContent(e.target.value)}
-            autoFocus
-          />
-          <input
-            type="date"
-            value={updatedTime.toISOString().split("T")[0]}
-            onChange={handleTimeChange}
-          />
-          <input
-            type="text"
-            value={updatedPerson}
-            onChange={(e) => setUpdatedPerson(e.target.value)}
-          />
-          <button onClick={handleSave}>儲存</button>
-          <button onClick={handleCancel}>取消</button>
-        </>
-      ) : (
-        <>
-          <span
-            style={{
-              textDecoration: todo.completed ? "line-through" : "none",
-            }}
-          >
-        {todo.content} - {formatDate(new Date(todo.time))} - {todo.person}
-          </span>
-          {!todo.deleted && (
-            <>
-              <button onClick={handleEdit}>編輯</button>
-              <button onClick={handleDelete}>刪除</button>
-            </>
-          )}
-        </>
-      )}
+
+      <>
+        <span
+          style={{
+            textDecoration: todo.completed ? "line-through" : "none",
+          }}
+        >
+          {todo.content} - {formatDate(new Date(todo.time))} - {todo.person}
+        </span>
+        {!todo.deleted && (
+          <>
+            <button onClick={handleEdit} className="editbtn">編輯</button>
+            <button onClick={handleDelete} className="deletebtn">刪除</button>
+          </>
+        )}
+      </>
+
       {todo.deleted && <button onClick={handleRestore}>復原</button>}
     </li>
   );
