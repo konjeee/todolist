6470d6f4e48aa94d0ea21031ea775920f421e723
劻 <LedoBodzaW0913@gmail.com>
2023-10-04 19:16:29 +0800

    update package

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/todolist/',
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    sourcemap: true,
+  },
 });
