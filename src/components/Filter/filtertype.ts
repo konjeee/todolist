4229eb54fde694d331d8dea2045a06492fac4116
劻 <LedoBodzaW0913@gmail.com>
2023-10-04 18:27:56 +0800

    initial

--- src/components/Filter/filtertype.ts
+++
@@ -1,6 +0,0 @@
-export type FilterType = "all" | "completed" | "uncompleted" | "deleted";
-
-export const FILTER_ALL: FilterType = "all";
-export const FILTER_COMPLETED: FilterType = "completed";
-export const FILTER_UNCOMPLETED: FilterType = "uncompleted";
-export const FILTER_DELETED: FilterType = "deleted";
--- src/types/todo.ts
+++
@@ -1,9 +0,0 @@
-export interface Todo {
-    id: string;
-    content: string;
-    time: Date;
-    person: string;
-    completed: boolean;
-    deleted: boolean;
-  }
-  
